8a65ce95bce4f9aa91b1acf935d01827faa0eaf0
sejongdeveloper <sejongdeveloper@gmail.com>
2021-05-27 20:17:42 +0900

    1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,36 +20,23 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("city1", "street", "10000"));
+            String qlString = "select m From Member as m";
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
+            String username;
+            if (username != null) {
+                String where = "where m.username like '%kim%'";
+                qlString + where;
+            }
 
-            member.getAddressesHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressesHistory().add(new AddressEntity("old2", "street", "10000"));
 
-            em.persist(member);
+            List<Member> result = em.createQuery(
+                    qlString,
+                    Member.class
+            ).getResultList();
 
-            em.flush();
-            em.clear();
-
-            System.out.println("========== START ==========");
-            Member findMember = em.find(Member.class, member.getId());
-
-            //homeCity -> newCity
-            //findMember.getHomeAddress().setCity("newCity");
-            //Address a = findMember.getHomeAddress();
-            //findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            //치킨 -> 한식
-            //findMember.getFavoriteFoods().remove("치킨");
-            //findMember.getFavoriteFoods().add("한식");
-
-            findMember.getAddressesHistory().remove(new AddressEntity("old1", "street", "10000"));
-            findMember.getAddressesHistory().add(new AddressEntity("newCity", "street", "10000"));
+            for (Member member : result) {
+                System.out.println("member = " + member);
+            }
 
             tx.commit();
         } catch (Exception e) {
